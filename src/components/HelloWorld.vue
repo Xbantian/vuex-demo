02664521cf622e2c218924d941c5978fbf548b13
bantian <bantian@qiuzhongyideMacBook-Pro.local>
2020-05-17 16:05:03 +0800

    $attrs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,19 +3,21 @@
     <h1>vuex练手</h1>
     <h2>模拟vuex的一些使用场景</h2>
     <h2>
-      <a href="https://vuex.vuejs.org/zh/guide/actions.html" target="_bank"
-        >文档</a
-      >
+      <a href="https://vuex.vuejs.org/zh/guide/actions.html" target="_bank">文档</a>
     </h2>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
 export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  @Prop() private msg!: string
+  created() {
+    // eslint-disable-next-line no-console
+    console.log(this.$attrs)
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld
+      :obj="obj"
+      class="qwe"
+      style="zxc"
+      data="asdasd"
+      alt="asdasd"
+      asd="asd"
+      msg="Welcome to Your Vue.js App"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
+
   components: {
-    HelloWorld
-  }
-};
+    HelloWorld,
+  },
+  data() {
+    return {
+      obj: {
+        a: 1,
+      },
+    }
+  },
+}
 </script>
